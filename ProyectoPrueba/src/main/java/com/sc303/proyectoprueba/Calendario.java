c5f1672e4d4a92c1e9fa76651c6fb0118db3fefc
IsaacRuizS <isaacruizse@gmail.com>
2023-06-22 15:32:56 -0600

    Simplificacion de calendario'

--- ProyectoPrueba/src/main/java/com/sc303/proyectoprueba/Calendario.java
+++ ProyectoPrueba/src/main/java/com/sc303/proyectoprueba/Calendario.java
@@ -8,26 +8,37 @@ import javax.swing.JOptionPane;
 
 public class Calendario {
  public static void mostrarCalendario() {
-        int year = 2023; // Año del calendario
-        int month = 5; // Mes del calendario (junio)
-//1,5
-        // Obtiene el nombre del mes y los días en él
-        String monthName = obtenerNombreMes(month);
-        int daysInMonth = obtenerDiasEnMes(year, month);
+        //Meses
+        String[] months = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                          "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};    
+        //Ultimo dia de cada mes del 2023
+        int[] daysPerMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        //solicitar el mes a consultar
+        int month = 5; 
+//1,5,7
 
-        // Crea una matriz de 6 filas y 7 columnas para representar el calendario
+        
+        // asignar el mes a mostrar y los dias
+        String monthName =  months[month - 1];
+        int daysInMonth =  daysPerMonth[month - 1];
+
+        // Crea una matriz de 6 semanas como filas y 7 columnas como dias para representar el calendario
         String[][] calendario = new String[6][7];
 
         // Rellena la matriz con los días del mes
         int day = 1;
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (i == 0 && j < obtenerDiaSemana(year, month, 1)) {
-                    calendario[i][j] = " ";
+        for (int week = 0; week < 6; week++) {
+            for (int days = 0; days < 7; days++) {
+                //valida si esta en la primera semana y la posicion del dia, si el dia es menor a la primer dia obtenido en obtenerDiaSemana entonces devolvera un espacio en blaco
+                if (week == 0 && days < obtenerDiaSemana(month, 1)) {
+                    calendario[week][days] = " ";
                 } else if (day > daysInMonth) {
-                    calendario[i][j] = " ";
+                    //si el dia es mayor a los dias en el mes entonces tambien escribira espacios en blanco
+                    calendario[week][days] = " ";
                 } else {
-                    calendario[i][j] = String.valueOf(day);
+                    //si no agrega los dias dentreo del arreglo en la posicion de la semana en dia.
+                    calendario[week][days] = String.valueOf(day);
                     day++;
                 }
             }
@@ -35,54 +46,33 @@ public class Calendario {
 
         // Crea el contenido del calendario con formato de tabla
         StringBuilder calendarioTable = new StringBuilder();
-        calendarioTable.append(String.format("   %s %d\n", monthName, year));
+        calendarioTable.append(String.format("   %s %d\n", monthName, 2023));
         calendarioTable.append("  D    L    M    M    J    V    S   \n");
 
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
-                if(i ==1){
-                    calendarioTable.append(String.format(" %3s ", calendario[i][j]));               
-                }else if(i == 0){
-                    calendarioTable.append(String.format(" %4s ", calendario[i][j]));               
-                }else{
-                    calendarioTable.append(String.format(" %2s ", calendario[i][j]));               
+        for (int week = 0; week < 6; week++) {
+            for (int days = 0; days < 7; days++) {
+                if (week == 1) {
+                    calendarioTable.append(String.format(" %3s ", calendario[week][days]));
+                } else if (week == 0) {
+                    calendarioTable.append(String.format(" %4s ", calendario[week][days]));
+                } else {
+                    calendarioTable.append(String.format(" %2s ", calendario[week][days]));
                 }
             }
-            calendarioTable.append("\n");
+                    calendarioTable.append("\n");
         }
+
+
 
         // Muestra el calendario en un cuadro de diálogo
         JOptionPane.showMessageDialog(null, calendarioTable.toString());
     }
+ 
 
-    // Obtiene el nombre del mes
-    public static String obtenerNombreMes(int month) {
-        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-                          "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-        return meses[month - 1];
-    }
-
-    // Obtiene la cantidad de días en un mes específico
-    public static int obtenerDiasEnMes(int year, int month) {
-        if (month == 2 && esAnioBisiesto(year)) {
-            return 29;
-        } else {
-            int[] diasPorMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            return diasPorMes[month - 1];
-        }
-    }
-
-    // Comprueba si un año es bisiesto
-    public static boolean esAnioBisiesto(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    }
-
-    // Obtiene el día de la semana para una fecha específica (0 - Domingo, 1 - Lunes, ...)
-    public static int obtenerDiaSemana(int year, int month, int day) {
-        int a = (14 - month) / 12;
-        int y = year - a;
-        int m = month + 12 * a - 2;
-        int d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
-        return d;
+    // Obtener el día de la semana correspondiente a esa fecha crada gracias al objeto LocalDate
+    public static int obtenerDiaSemana(int month, int day) {
+        LocalDate date = LocalDate.of(2023, month, day);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek.getValue();
     }
 }
